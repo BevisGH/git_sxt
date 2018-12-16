dc74d5c40681c364bcfc9c742056a53014c25c6a
BevisGH <824603352@qq.com>
2018-12-16 20:21:25 +0800

    test

--- TestGit/src/cn/gzsxt/git/test/MyGit.java
+++ TestGit/src/cn/gzsxt/git/test/MyGit.java
@@ -4,7 +4,7 @@ public class MyGit {
 	public static void main(String[] args) {
 		int b = 3;
 		int a = 2;
-		System.out.println("Test git....... secondly");
+		System.out.println("Test git....... secondly"+a+"+"+b);
 	}
 	public void test2(){
 		System.out.println("this is so.......");
