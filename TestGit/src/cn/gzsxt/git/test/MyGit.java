e9350af07d0968de482232afa449b4ad22175bee
BevisGH <824603352@qq.com>
2018-12-16 20:14:58 +0800

    ct

--- TestGit/src/cn/gzsxt/git/test/MyGit.java
+++ TestGit/src/cn/gzsxt/git/test/MyGit.java
@@ -2,6 +2,7 @@ package cn.gzsxt.git.test;
 
 public class MyGit {
 	public static void main(String[] args) {
+		int b = 3;
 		System.out.println("Test git....... secondly");
 	}
 	public void test2(){
