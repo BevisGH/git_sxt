5e3b6fd09dcae14cb18eaea260ed5ab01b471f71
BevisGH <45914649+BevisGH@users.noreply.github.com>
2018-12-16 19:57:46 +0800

    Update MyGit.java
    在中央仓库修改MyGit,新增test2方法

--- TestGit/src/cn/gzsxt/git/test/MyGit.java
+++ TestGit/src/cn/gzsxt/git/test/MyGit.java
@@ -4,4 +4,7 @@ public class MyGit {
 	public static void main(String[] args) {
 		System.out.println("Test git....... secondly");
 	}
+	public void test2(){
+		System.out.println("this is so.......");
+	}
 }
